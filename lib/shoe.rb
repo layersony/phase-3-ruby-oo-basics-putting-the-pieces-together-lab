1167b49015d95c1abdc300aa17c63c0bf5cedc61
layersony <layersony@gmail.com>
2022-07-30 18:38:28 +0300

    commit tree 01b5f6975b15a05ba19b8c0fce00e6fb5d5186b9

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,15 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :brand
+  attr_accessor :color
+  attr_accessor :size
+  attr_accessor :material
+  attr_accessor :condition = "Your shoe is as good as new!"
+  
+  def initialize(brand)
+    @brand = brand
+  end 
+
+  def cobble 
+    puts @condition
+  end 
+end
